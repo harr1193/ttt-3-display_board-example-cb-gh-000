c995d9be6cf35ec8f0537bfc00891cfcdbb839f7
Kyle Harris <k_harris95@live.com>
2018-01-06 23:49:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  row_end = [-----------]
+  row_end = "-----------"
   puts "   |   |   "
   puts row_end
   puts "   |   |   "
